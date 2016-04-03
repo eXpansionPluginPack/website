3ff1a02a0d70fd4820a6ead9da6eedc3b382d3fe
reaby <petri.jarvisalo@gmail.com>
2016-04-03 13:58:07 +0300

    add new release

--- pages/download.php
+++ pages/download.php
@@ -4,10 +4,25 @@
         <h1 class="uk-heading">Downloads</h1>
         <p>Here you can find all eXpansion releases! Checkout the Documentation for installation instructions. And see forums for more info about the release.</p>
    <hr class="uk-grid-divider">
-        <div class="uk-grid">
+   
+   
+   <div class="uk-grid">		
+   <div class="uk-width-1-6 uk-text-right">
+                <h2>Current (1.0.0.5)</h2>
+                <a href="http://files.oliver-decramer.com/data/downloads/pers/mp/ml/eXpansion/eXpansion-1.0.0.5.zip" class="uk-button uk-button-success">Download</a>
+            </div>
+			<div class="uk-width-5-6">
+
+                <p>Released at 3.4.2016<br/>
+                    see <a href="https://forum.maniaplanet.com/viewtopic.php?f=518&t=37781">forum posting</a> for changelog, migrating and more info.</p>
+            </div>
+			 </div>
+        <hr class="uk-grid-divider">
+        <div class="uk-grid">						
+			
             <div class="uk-width-1-6 uk-text-right">
-                <h2>Current (0.9.8)</h2>
-                <a href="http://server1.oliver-decramer.com/MP/exp/eXpansion-0.9.8.7.zip" class="uk-button uk-button-success">Download</a>
+                <h2>0.9.8</h2>
+                <a href="http://server1.oliver-decramer.com/MP/exp/eXpansion-0.9.8.7.zip" class="uk-button">Download</a>
             </div>
             <div class="uk-width-5-6">
 
@@ -16,9 +31,7 @@
             </div>
 
         </div>
-        <hr class="uk-grid-divider">
-		
-	  <hr class="uk-grid-divider">
+     	  <hr class="uk-grid-divider">
         <div class="uk-grid">
             <div class="uk-width-1-6 uk-text-right">
                 <h2>0.9.6</h2>
